29a79d5fac8b63da996ab0dcd48fdabe486f148b
Bhanuprakash842 <127642353+Bhanuprakash842@users.noreply.github.com>
2024-11-08 00:01:44 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -36,10 +36,10 @@ const app = express();
 
 //For the dynamic routes we give as 
 
-app.get("/user/:userId/:name/:password",(req,res)=>{
-    console.log(req.params);
-    res.send({firstName:"Bhanu",lastName:"Prakash"});
-});
+// app.get("/user/:userId/:name/:password",(req,res)=>{
+//     console.log(req.params);
+//     res.send({firstName:"Bhanu",lastName:"Prakash"});
+// });
 
 
 
@@ -49,6 +49,38 @@ app.get("/user/:userId/:name/:password",(req,res)=>{
 //We can also group things (bc)? which means without bc also the route works
 //We can also use the Regex like /a/ which means any route containing a will go to that route 
 // /.*fly$/ - It should start with any word but end with fly only without any additional letters.
+
+// app.use("/route",rH1,rH2,rH3,rH4,rH5);
+
+app.use("/user",(req,res,next)=>{
+    //Route Handler
+    //res.send("Route Handler 1");
+    console.log("Router Handler 1");
+    next();//We will get an error again as we are responding with two responses
+    // res.send("Response!!");//When the next function also sends the response then we will get the error in the console because we cannot change the response once the response is sent 
+    // next();
+},
+(req,res,next)=>{
+    console.log("Handling the route 2");
+    // res.send("2nd Response!!");
+    next();
+},
+(req,res,next)=>{
+    console.log("Handling the route 3");
+    // res.send("3rd Response!!");
+    next();
+},
+(req,res,next)=>{
+    console.log("Handling the route 4");
+    // res.send("4th Response!!");
+    next();
+},
+(req,res,next)=>{
+    console.log("Handling the route 5");
+    res.send("5th Response!!");
+});
+
+
 app.listen(7777,() =>{
     console.log("Server is successfully listening on port 7777...")
 });
